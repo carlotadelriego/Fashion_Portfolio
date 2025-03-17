e47d6b1d55dc19cb6aa9c58e45cd3d25ef2401c1
carlotadelriego <carlota.fernandez.01@uie.edu>
2025-03-17 18:31:00 +0100

    I start with the preproccesing (tokenize, delete stopwords and normalization)

--- proyecto.py
+++ proyecto.py
@@ -8,13 +8,15 @@ import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import spacy
+from spacy.lang.es.stop_words import STOP_WORDS
+import requests
+from bs4 import BeautifulSoup
 
 
 # Cargamos los pdf recolectados
 files_data = [
-    '/Users/carlotafernandez/Desktop/Code/FASHION/Fashion_Portfolio-1/christianDior.pdf',
-    '/Users/carlotafernandez/Desktop/Code/FASHION/Fashion_Portfolio-1/PMM.pdf',
-    '/Users/carlotafernandez/Desktop/Code/FASHION/Fashion_Portfolio-1/Sistema_Moda.pdf'
+    '/Users/carlotafernandez/Desktop/Code/FASHION/Fashion_Portfolio-1/Dior_english.pdf'
 ]
 
 # Web scraping
@@ -24,13 +26,32 @@ url = [
     'https://www.elle.com/culture/music/a63324386/lady-gaga-mayhem-interview-2025/'
 ]
 
-# Función para hacer web scraping y extraer enlaces de una página web
-def scrape_links_from_url(url):
-    import requests
-    from bs4 import BeautifulSoup
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    links = []
-    for link in soup.find_all('a'):
-        links.append(link.get('href'))
-    return links
+# Función para hacer web scraping y extraer los textos de los enlaces
+def web_scraping(url):
+    texts = []
+    for link in url:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        text = soup.get_text()
+        texts.append(text)
+    return texts
+
+scraped_texts = web_scraping(url)
+print(scraped_texts)
+
+
+###### Preprocesamiento del lenguaje
+# Limpieza del texto
+# 1. Tokenización con spaCy 
+nlp = spacy.load("en_core_news_sm")
+documents = files_data + scraped_texts
+tokenized_documents = [nlp(document) for document in documents]
+print(tokenized_documents)
+
+
+# 2. Delete stopwords
+
+
+
+
+# 3. Normalization
